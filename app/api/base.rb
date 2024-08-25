1c7ba23b55a917d10b6920f6986e18a16e324bf9
myselfmandeep <mandeeprawat2516@outlook.com>
2024-08-25 18:39:58 +0530

    added grape structure

---
+++ app/api/base.rb
@@ -0,0 +1,16 @@
+class Base < Grape::API
+  PREFIX = 'api'
+  format :json
+  
+  mount V1::Base => "/#{V1::Base::VERSION}"
+
+  get '/ping' do
+    {ping: 'pong'}
+  end
+
+  desc "Handle Invalid Paths"
+  route :any, "*path" do
+    error! "Route is not found", 404
+  end
+  
+end
---
+++ app/api/v1/base.rb
@@ -0,0 +1,7 @@
+module V1
+  class Base < Grape::API
+    VERSION = 'v1'
+
+    
+  end
+end
